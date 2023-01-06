406ab0df51c4210fada1d9513ec8b0460bc751d9
Mike <mwjones2186@gmail.com>
2023-01-06 05:28:13 -0800

    copied some boilerplate nav from wedding project. file structure bones in place. obv some adjustments need to be made but over all strong start. react app is working but the home page and nav bars are not displaying. Need to trouble shoot the issues there.

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # platform-technologies
 Platform Technologies website. Medical device company
-
-
-checkout this cool stuff we have here
-
-check out our readme
--- platform-technologies/src/App.js
+++ platform-technologies/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import './assets/css/App.css'
+import Navigation from './components/Navigation';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Packages from './pages/Packages';
+import Venue from './pages/Venue';
+import Gallery from "./pages/Gallery";
+import Footer from "./pages/Footer";
+import { Nav } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Navigation />
+
+      <Routes>
+        <Route path="/" element={< Home />}></Route>
+        <Route path="/venue" element={< Venue />}></Route>
+        <Route path="/packages" element={< Packages />}></Route>
+        <Route path="/about" element={< About />}></Route>
+        <Route path="/contact" element={< Contact />}></Route>
+        <Route path="/gallery" element={< Gallery />}></Route>
+      </Routes>
+
+      <Footer />
     </div>
   );
 }
