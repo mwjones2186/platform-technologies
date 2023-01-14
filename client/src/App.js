c9a0c11f818ad2d7495bf86dd085ac05ea4a5224
Jonsey <mwjones2186@gmail.com>
2023-01-14 10:06:01 -0800

    my branch is a mess

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import './assets/css/App.css'
 import Navigation from './components/Navigation';
-import About from './pages/About';
-import Contact from './pages/Contact';
+// import About from './pages/About';
+// import Contact from './pages/Contact';
 import Home from './pages/Home';
-import Packages from './pages/Packages';
-import Venue from './pages/Venue';
-import Gallery from "./pages/Gallery";
-import Footer from "./pages/Footer";
+// import Packages from './pages/Packages';
+// import Venue from './pages/Venue';
+// import Gallery from "./pages/Gallery";
+// import Footer from "./pages/Footer";
 import { Nav } from "react-bootstrap";
 
 function App() {
@@ -18,14 +18,14 @@ function App() {
 
       <Routes>
         <Route path="/" element={< Home />}></Route>
-        <Route path="/venue" element={< Venue />}></Route>
+        {/* <Route path="/venue" element={< Venue />}></Route>
         <Route path="/packages" element={< Packages />}></Route>
         <Route path="/about" element={< About />}></Route>
         <Route path="/contact" element={< Contact />}></Route>
-        <Route path="/gallery" element={< Gallery />}></Route>
+        <Route path="/gallery" element={< Gallery />}></Route> */}
       </Routes>
 
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
