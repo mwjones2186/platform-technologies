ebb50bf490f2f66f5f9108e37bc0458e4f11a661
Koda Passey <kodapassey@charter.net>
2023-01-14 09:54:39 -0800

    navbar close to being finished, still needs some things worked on

--- platform-technologies/src/App.js
+++ platform-technologies/src/App.js
@@ -4,11 +4,9 @@ import Navigation from './components/Navigation';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
-import Packages from './pages/Packages';
-import Venue from './pages/Venue';
-import Gallery from "./pages/Gallery";
+import Documents from './pages/Documents';
+import Technology from './pages/Technology';
 import Footer from "./pages/Footer";
-import { Nav } from "react-bootstrap";
 
 function App() {
   return (
@@ -18,14 +16,13 @@ function App() {
 
       <Routes>
         <Route path="/" element={< Home />}></Route>
-        <Route path="/venue" element={< Venue />}></Route>
-        <Route path="/packages" element={< Packages />}></Route>
+        <Route path="/documents" element={< Documents />}></Route>
+        <Route path="/technology" element={< Technology />}></Route>
         <Route path="/about" element={< About />}></Route>
         <Route path="/contact" element={< Contact />}></Route>
-        <Route path="/gallery" element={< Gallery />}></Route>
       </Routes>
 
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
