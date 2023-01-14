44b4321d1f5d789651a5b9b25b8baa23ad62b036
Jonsey <mwjones2186@gmail.com>
2023-01-14 09:59:14 -0800

    broken code

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import { Route, Routes } from "react-router-dom";
+import './assets/css/App.css'
+import Navigation from './components/Navigation';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Packages from './pages/Packages';
+import Venue from './pages/Venue';
+import Gallery from "./pages/Gallery";
+import Footer from "./pages/Footer";
+import { Nav } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+
+      <Navigation />
+
+      <Routes>
+        <Route path="/" element={< Home />}></Route>
+        <Route path="/venue" element={< Venue />}></Route>
+        <Route path="/packages" element={< Packages />}></Route>
+        <Route path="/about" element={< About />}></Route>
+        <Route path="/contact" element={< Contact />}></Route>
+        <Route path="/gallery" element={< Gallery />}></Route>
+      </Routes>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
