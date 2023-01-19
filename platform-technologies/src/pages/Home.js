f7eb1e8111268b6a88ae5b17449bb104dcd18d9b
Jonsey <mwjones2186@gmail.com>
2023-01-19 11:08:40 -0800

    merge

--- platform-technologies/src/pages/Home.js
+++ platform-technologies/src/pages/Home.js
@@ -16,12 +16,7 @@ export default function Home() {
                 </img>
 
                 <div className='mainText'>
-<<<<<<< HEAD
                     <h2><span className='futureTxt'>THE FUTURE</span> of Minimally Invasive Surgery</h2>  
-=======
-                    <h2><span className='futureTxt'>THE FUTURE</span> of Minimally Invasive Surgery</h2>
-                    <button>Watch a Video</button>
->>>>>>> accb72b9a600526e9e076ec44f8ed783d4d639ff
                 </div>
                 <VisibotModal/>
                 
